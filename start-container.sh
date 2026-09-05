db474407b6301387668969d41d46edbcf88bab45
hadi syaputra <hadisyaputra.sumsel@gmail.com>
2026-09-05 12:15:03 +0700

    Enable debug mode and DB wait loop to resolve 500 server error

--- start-container.sh
+++ start-container.sh
@@ -2,6 +2,13 @@
 # Ensure no leftover apache PID
 rm -f /var/run/apache2/apache2.pid
         
+# Wait for database to be ready
+RETRIES=15
+until php artisan db:monitor > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
+  echo "Waiting for database server, $((RETRIES--)) remaining attempts..."
+  sleep 2
+done
+
 # Run Laravel commands
 php artisan migrate --force || true
 php artisan optimize:clear || true
